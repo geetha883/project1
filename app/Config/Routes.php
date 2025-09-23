31cd01ebb48a0a5aa5b0256995030e7abf3493c1
geetha883 <geetha272787@gmail.com>
2025-09-23 18:36:32 +0530

    login form

---
+++ app/Config/Routes.php
@@ -0,0 +1,10 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+//$routes->setDefaultControllers('Users');
+$routes->get('/', 'Users::index');
+$routes->match(['get','post'],'register', 'Users::register');
